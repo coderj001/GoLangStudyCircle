0696f11d5b9534494a642d5660a21816350a60be
Raju Ghorai <amirajubolchi001@gmail.com>
2022-12-27 19:23:51 +0530

    day 4 hello api

---
+++ day04/helloapi/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHelloHandler(t *testing.T) {
+	rec := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/hello", nil)
+
+	helloHandler(rec, r)
+	body := rec.Result().Body
+	data, _ := io.ReadAll(body)
+	assert.Equal(t, string(data), "hello")
+}
