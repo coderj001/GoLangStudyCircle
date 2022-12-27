67803b546c6c2d0234b6fcb069ef2aaaca59f13f
Raju Ghorai <amirajubolchi001@gmail.com>
2022-12-27 16:39:40 +0530

    update account code and testcase

--- day02/bankAccount/account/account.go
+++ day02/bankAccount/account/account.go
@@ -2,18 +2,56 @@ package account
 
 // TDD Bank Account app
 
-type Account struct {
+type Account interface {
+	Withdraw(amount float64)
+	CanWithdraw(amount float64) bool
+	GetIdentifier() string
+}
+type DebitCardAccount struct {
 	balance float64
 }
 
-func (acc *Account) GetBalance() float64 {
-	return acc.balance
+func (dca *DebitCardAccount) Withdraw(amount float64) {
+	dca.balance -= amount
 }
 
-func (acc *Account) Deposit(amount float64) {
-	acc.balance += amount
+func (dca *DebitCardAccount) CanWithdraw(amount float64) bool {
+	return dca.balance >= amount
 }
 
-func (acc *Account) Withdraw(amount float64) {
-	acc.balance -= amount
+func (dca *DebitCardAccount) GetIdentifier() string {
+	return "DEBIT_CARD_ACCOUNT"
+}
+
+type CreditCardAccount struct {
+	balance float64
+	limit   float64
+}
+
+func (cca *CreditCardAccount) Withdraw(amount float64) {
+	cca.balance -= amount
+}
+
+func (cca *CreditCardAccount) CanWithdraw(amount float64) bool {
+	return cca.balance+cca.limit >= amount
+}
+
+func (cca *CreditCardAccount) GetIdentifier() string {
+	return "CREDIT_CARD_ACCOUNT"
+}
+
+type PaytmWallet struct {
+	balance float64
+}
+
+func (pw *PaytmWallet) Withdraw(amount float64) {
+	pw.balance -= amount
+}
+
+func (pw *PaytmWallet) CanWithdraw(amount float64) bool {
+	return pw.balance >= amount
+}
+
+func (pw *PaytmWallet) GetIdentifier() string {
+	return "PAYTM_WALLET"
 }
--- day02/bankAccount/account/account_test.go
+++ day02/bankAccount/account/account_test.go
@@ -1,28 +1,103 @@
 package account
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestGetBalance(t *testing.T) {
-	acc := Account{balance: 100}
+func TestDebitCardAccount_Withdraw(t *testing.T) {
+	dca := DebitCardAccount{balance: 100}
 
-	assert.Equal(t, float64(100), acc.GetBalance())
+	dca.Withdraw(50)
+
+	if dca.balance != 50 {
+		t.Errorf("Expected balance to be 50, got %v", dca.balance)
+	}
 }
 
-func TestSuccessfulDeposit(t *testing.T) {
-	acc := Account{balance: 400}
+func TestDebitCardAccount_CanWithdraw(t *testing.T) {
+	dca := DebitCardAccount{balance: 100}
 
-	(&acc).Deposit(100)
+	if !dca.CanWithdraw(50) {
+		t.Error("Expected CanWithdraw to return true")
+	}
 
-	assert.Equal(t, float64(500), acc.GetBalance())
+	if dca.CanWithdraw(200) {
+		t.Error("Expected CanWithdraw to return false")
+	}
 }
 
-func TestSuccessfulWithdrawal(t *testing.T) {
-	acc := Account{balance: 500}
+func TestDebitCardAccount_GetIdentifier(t *testing.T) {
+	dca := DebitCardAccount{}
 
-	acc.Withdraw(200)
+	if dca.GetIdentifier() != "DEBIT_CARD_ACCOUNT" {
+		t.Errorf("Expected GetIdentifier to return 'DEBIT_CARD_ACCOUNT', got %v", dca.GetIdentifier())
+	}
+}
 
-	assert.Equal(t, float64(300), acc.GetBalance())
+func TestCreditCardAccount_Withdraw(t *testing.T) {
+	cca := CreditCardAccount{balance: 100, limit: 200}
+	cca.Withdraw(50)
+
+	if cca.balance != 50 {
+		t.Errorf("Expected balance to be 50, got %v", cca.balance)
+	}
+
+}
+
+func TestCreditCardAccount_CanWithdraw(t *testing.T) {
+	cca := CreditCardAccount{balance: 100, limit: 200}
+
+	if !cca.CanWithdraw(50) {
+		t.Error("Expected CanWithdraw to return true")
+	}
+
+	if !cca.CanWithdraw(300) {
+		t.Error("Expected CanWithdraw to return true")
+	}
+
+	if cca.CanWithdraw(400) {
+		t.Error("Expected CanWithdraw to return false")
+	}
+
+}
+
+func TestCreditCardAccount_GetIdentifier(t *testing.T) {
+	cca := CreditCardAccount{}
+
+	if cca.GetIdentifier() != "CREDIT_CARD_ACCOUNT" {
+		t.Errorf("Expected GetIdentifier to return 'CREDIT_CARD_ACCOUNT', got %v", cca.GetIdentifier())
+	}
+
+}
+
+func TestPaytmWallet_Withdraw(t *testing.T) {
+	pw := PaytmWallet{balance: 100}
+
+	pw.Withdraw(50)
+
+	if pw.balance != 50 {
+		t.Errorf("Expected balance to be 50, got %v", pw.balance)
+	}
+
+}
+
+func TestPaytmWallet_CanWithdraw(t *testing.T) {
+	pw := PaytmWallet{balance: 100}
+
+	if !pw.CanWithdraw(50) {
+		t.Error("Expected CanWithdraw to return true")
+	}
+
+	if pw.CanWithdraw(200) {
+		t.Error("Expected CanWithdraw to return false")
+	}
+
+}
+
+func TestPaytmWallet_GetIdentifier(t *testing.T) {
+	pw := PaytmWallet{}
+	if pw.GetIdentifier() != "PAYTM_WALLET" {
+		t.Errorf("Expected GetIdentifier to return 'PAYTM_WALLET', got %v", pw.GetIdentifier())
+	}
+
 }
