97ef2d6c61b767269b22da02eda4eac046dec240
Raju Ghorai <amirajubolchi001@gmail.com>
2022-12-27 22:23:03 +0530

    code updated

--- day04/bookapi/main.go
+++ day04/bookapi/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,19 +13,47 @@ type Book struct {
 	Price float64 `json:"price"`
 }
 
-var books []Book = [{Book{Id: 1, Title: "Some Title1", Price: 311.22}]
+var books []Book = []Book{Book{Id: 1, Title: "Some Title1", Price: 311.22}}
 
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
 	router.POST("/books", addBooks)
-	router.DELETE("/books", deleteBooks)
+	router.DELETE("/books/:id", deleteBooks)
 
 	router.Run()
 }
 
 func getBooks(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{})
+	c.JSON(http.StatusOK, books)
 }
-func addBooks(c *gin.Context)    {}
-func deleteBooks(c *gin.Context) {}
+
+func addBooks(c *gin.Context) {
+	var book Book
+
+	if err := c.ShouldBindJSON(&book); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	books = append(books, book)
+
+	c.JSON(http.StatusCreated, book)
+}
+
+func deleteBooks(c *gin.Context) {
+	bookID, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	for i, book := range books {
+		if book.Id == bookID {
+			books = append(books[:i], books[i+1:]...)
+			break
+		}
+	}
+	c.Status(http.StatusNoContent)
+}
